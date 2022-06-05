bf14776f6c2d57d9f53270165d05fa993efaed89
Cavid <hcavid386@gmail.com>
2022-06-05 19:36:37 +0400

    :bomb:

---
+++ Services/Catalog/FreeCourse.Service.Catalog/Models/Course.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FreeCourse.Service.Catalog.Models
+{
+    public class Course
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+        public string Picture { get; set; }
+
+        [BsonRepresentation(BsonType.Decimal128)]
+        public Decimal Price { get; set; }
+
+        public string UserId { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime CreatedTime { get; set; }
+    }
+}
